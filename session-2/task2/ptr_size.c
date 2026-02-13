18b07a830da6c6a21f159c6ce2b5933f1cd12c53
aliberenadamcil <berenadamcil@gmail.com>
2026-02-13 10:39:40 +0000

    .

--- session-2/task2/ptr_size.c
+++ session-2/task2/ptr_size.c
@@ -9,8 +9,10 @@
 int main( void ) {
 
     int *testIntPtr;
+    int **testIntPtrPtr;
 
     printf("Int pointer size is %ld bytes\n",sizeof(testIntPtr));
+    printf("Int pointer to pointer size is %ld bytes\n",sizeof(testIntPtrPtr));
 
     return 0;
 }
